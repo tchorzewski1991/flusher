a4ede747ac2e4550c4c55143a6fdd2b9d60f0568
tchorzewski1991 <rafal.tchorzewski@airhelp.com>
2021-05-26 00:12:16 +0200

    Add first prototype of Agent API

--- flusher/agent.go
+++ flusher/agent.go
@@ -1,3 +1,23 @@
 package flusher
 
-type Agent struct{}
+type Agent struct {
+	active bool
+}
+
+func NewAgent() *Agent {
+	return &Agent{}
+}
+
+func (a *Agent) Start() error {
+	a.active = true
+	return nil
+}
+
+func (a *Agent) Stop() error {
+	a.active = false
+	return nil
+}
+
+func (a *Agent) Active() bool {
+	return a.active
+}
--- flusher/agent_test.go
+++ flusher/agent_test.go
@@ -3,10 +3,27 @@ package flusher_test
 import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/tchorzewski1991/flusher/flusher"
 )
 
 var _ = Describe("Agent", func() {
-	It("should work", func() {
-		Expect(true).To(Equal(true))
+	var agent *flusher.Agent
+
+	BeforeEach(func() {
+		agent = flusher.NewAgent()
+	})
+
+	Context("Start", func() {
+		It("starts agent", func() {
+			Expect(agent.Start()).NotTo(HaveOccurred())
+			Expect(agent.Active()).To(BeTrue())
+		})
+	})
+
+	Context("Stop", func() {
+		It("stops agent", func() {
+			Expect(agent.Stop()).NotTo(HaveOccurred())
+			Expect(agent.Active()).To(BeFalse())
+		})
 	})
 })
